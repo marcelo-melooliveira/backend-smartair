ee2a0264bb153e016c07ca01323e2146e5ea8903
Marcelo Melo <mm.ifce@gmail.com>
2020-03-18 15:09:30 -0300

    route feedback ok

--- src/controllers/ReturnFeedbackController.js
+++ src/controllers/ReturnFeedbackController.js
@@ -2,9 +2,8 @@
 module.exports = {
    return_feedback(req, res) {
     console.log("Chegou um feedback");
-    
-    console.log(req.body.user);
-    req.io.emit(req.body.user, {verifica: true});
+    console.log(req.body);
+    req.io.emit(req.body.user, req.body);
     res.status(200).send("ok");
   }
 };
